2e1dd7ee4860c544205494fd6bd6c35122710291
Constantin Rack <constantin@rack.li>
2013-12-27 22:56:56 +0100

    Fix failing Example73Test

--- src/main/java/co/nstant/in/cbor/model/ByteString.java
+++ src/main/java/co/nstant/in/cbor/model/ByteString.java
@@ -1,37 +1,35 @@
 package co.nstant.in.cbor.model;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 public class ByteString extends ChunkableDataItem {
 
-	private final byte[] bytes;
+    private final byte[] bytes;
 
-	public ByteString(byte[] bytes) {
-		super(MajorType.BYTE_STRING);
-		Objects.requireNonNull(bytes);
-		this.bytes = bytes;
-	}
+    public ByteString(byte[] bytes) {
+        super(MajorType.BYTE_STRING);
+        this.bytes = bytes;
+    }
 
-	public byte[] getBytes() {
-		return bytes;
-	}
+    public byte[] getBytes() {
+        return bytes;
+    }
 
-	@Override
-	public boolean equals(Object object) {
-		if (this == object) {
-			return true;
-		}
-		if (object instanceof ByteString) {
-			ByteString other = (ByteString) object;
-			return Arrays.equals(bytes, other.bytes);
-		}
-		return false;
-	}
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object instanceof ByteString) {
+            ByteString other = (ByteString) object;
+            return Arrays.equals(bytes, other.bytes);
+        }
+        return false;
+    }
 
-	@Override
-	public int hashCode() {
-		return bytes.hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return bytes.hashCode();
+    }
 
 }
--- src/main/java/co/nstant/in/cbor/model/UnicodeString.java
+++ src/main/java/co/nstant/in/cbor/model/UnicodeString.java
@@ -2,37 +2,41 @@ package co.nstant.in.cbor.model;
 
 public class UnicodeString extends ChunkableDataItem {
 
-	private final String string;
+    private final String string;
 
-	public UnicodeString(String string) {
-		super(MajorType.UNICODE_STRING);
-		this.string = string;
-	}
+    public UnicodeString(String string) {
+        super(MajorType.UNICODE_STRING);
+        this.string = string;
+    }
 
-	@Override
-	public String toString() {
-		return string;
-	}
+    @Override
+    public String toString() {
+        return string;
+    }
 
-	@Override
-	public boolean equals(Object object) {
-		if (this == object) {
-			return true;
-		}
-		if (object instanceof UnicodeString) {
-			UnicodeString other = (UnicodeString) object;
-			if (string == null) {
-				return other == null;
-			} else {
-				return string.equals(other.string);
-			}
-		}
-		return false;
-	}
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object instanceof UnicodeString) {
+            UnicodeString other = (UnicodeString) object;
+            if (string == null) {
+                return other.string == null;
+            } else {
+                return string.equals(other.string);
+            }
+        }
+        return false;
+    }
 
-	@Override
-	public int hashCode() {
-		return string.hashCode();
-	}
+    @Override
+    public int hashCode() {
+        if (string == null) {
+            return 0;
+        } else {
+            return string.hashCode();
+        }
+    }
 
 }
