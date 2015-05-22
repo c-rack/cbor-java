19cfbe32caa1ac1ce192b1383bc8c82d7618803f
Constantin Rack <constantin@rack.li>
2015-05-22 18:00:32 +0200

    Basic implementation of a LanguageTaggedString

---
+++ src/main/java/co/nstant/in/cbor/model/LanguageTaggedString.java
@@ -0,0 +1,25 @@
+package co.nstant.in.cbor.model;
+
+import java.util.Objects;
+
+public class LanguageTaggedString extends Array {
+
+    public LanguageTaggedString(String language, String string) {
+        this(new UnicodeString(language), new UnicodeString(string));
+    }
+
+    public LanguageTaggedString(UnicodeString language, UnicodeString string) {
+        setTag(38);
+        add(Objects.requireNonNull(language));
+        add(Objects.requireNonNull(string));
+    }
+
+    public UnicodeString getLanguage() {
+        return (UnicodeString) getDataItems().get(0);
+    }
+
+    public UnicodeString getString() {
+        return (UnicodeString) getDataItems().get(1);
+    }
+
+}
